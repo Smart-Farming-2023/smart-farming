475bed16c4548ca1870ecde5dca75e20698231ae
MilanRibadiya <milanribadiya10008@gmail.com>
2023-03-03 16:28:58 +0530

    Add Home Page Models

--- mongooes.connection.js
+++ mongooes.connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const mongoURL="mongodb+srv://root:root@cluster0.fryz3qk.mongodb.net/smart-farming?retryWrites=true&w=majority";
 const localURL="mongodb://127.0.0.1:27017/smart-farming";
 mongoose
-  .connect(localURL)
+  .connect(mongoURL)
   .then(() => {
     console.log("Connect Successful");
   })
